78fa31ef9b6e0a4b0548a6b01b11e6a13a3ad1ce
typd <othertang@gmail.com>
2013-03-12 10:59:14 +0800

    Remove useless class

--- basicplib/util/mailer.py
+++ basicplib/util/mailer.py
@@ -6,40 +6,31 @@ from email.mime.base import MIMEBase
 from email.utils import formatdate
 from email import encoders as Encoders
 
-class Mailer(object):
-    def __init__(self, smtp_host, send_from=None, subject_prefix=None):
-        self.smtp = smtplib.SMTP(smtp_host)
-        self.send_from = send_from
-        self.subject_prefix = subject_prefix
-
-
-    def send(self, send_to, subject, text, files=None, text_type="plain"):
-        """ 
-        based on http://snippets.dzone.com/posts/show/2038
-        attachments can be specified as a list of strings [file_name, ...] or a list of tuples
-        containing intended names and file data [(file_name, data), ...]
-        """
-        assert type(send_to) == list
-        msg = MIMEMultipart()
-        msg['From'] = self.send_from 
-        msg['To'] = ', '.join(send_to)
-        msg['Date'] = formatdate(localtime=True)
-        if self.subject_prefix:
-            msg['Subject'] = self.subject_prefix + " " + subject
+def send(smtp_host, send_from, send_to, subject, text,
+        files=None, text_type="plain"):
+    """ 
+    based on http://snippets.dzone.com/posts/show/2038
+    attachments can be specified as a list of strings [file_name, ...] or a list of tuples
+    containing intended names and file data [(file_name, data), ...]
+    """
+    assert type(send_to) == list
+    msg = MIMEMultipart()
+    msg['From'] = send_from 
+    msg['To'] = ', '.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text, text_type))
+    for _file in files:
+        if type(_file) is tuple:
+            name, data = _file
         else:
-            msg['Subject'] = subject
-        msg.attach(MIMEText(text, text_type))
-        for _file in files:
-            if type(_file) is tuple:
-                name, data = _file
-            else:
-                name = _file
-                data = open(name,"rb").read()
-            part = MIMEBase('application', "octet-stream")
-            part.set_payload(data)
-            Encoders.encode_base64(part)
-            part.add_header('Content-Disposition', 'attachment; filename="{}"'
-                    .format(os.path.basename(name)))
-            msg.attach(part)
-        smtp = smtplib.SMTP(self.smtp)
-        smtp.sendmail(self.send_from, send_to, msg.as_string())
+            name = _file
+            data = open(name,"rb").read()
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(data)
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="{}"'
+                .format(os.path.basename(name)))
+        msg.attach(part)
+    smtp = smtplib.SMTP(smtp_host)
+    smtp.sendmail(send_from, send_to, msg.as_string())
